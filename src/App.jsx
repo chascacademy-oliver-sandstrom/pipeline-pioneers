d44042229f42472b24addbd71c7ae7b3e6f3cc68
crewiz <s3salas@gmail.com>
2023-09-20 13:41:32 +0200

    added !

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>hosting är kul!</h2>
+      <h2>hosting är kul!!</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
